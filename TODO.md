54964f6de965f2191359efb1e658c6d77ce5f133
Zoee Silcock <mrzoee@gmail.com>
2024-02-02 11:55:20 +0100

    Start looking at row generation

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# Todo
+
+## Data types
+* Sequencial number
+* Date
+* Random text
+* Comma separated text
+* Hierarcical data (A, A1, A2, B, B1, B2)
+* Reference to other row
+
+# Done
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+use time::format_description::well_known::Iso8601;
+use time::Date;
+
 fn main() {
-    println!("Hello, world!");
+    let mut row = HashMap::new();
+    let date = Date::from_ordinal_date(2024, 33);
+    let date_string = date.unwrap().format(&Iso8601::DATE).unwrap();
+
+    row.insert("id".to_string(), "1".to_string());
+    row.insert("date".to_string(), date_string);
+
+    for (key, value) in &row {
+        println!("{}: {}", key, value);
+    }
 }
